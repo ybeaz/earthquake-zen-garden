0d994a21d4472fc1a890e4eb5acea092c2ce9ba2
R1 <ybeaz@yahoo.com>
2021-02-05 19:34:09 +0500

    Add solution v1.0

--- babel.config.js
+++ babel.config.js
@@ -1,41 +1,49 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: [
       [
         '@babel/preset-env',
         {
-          'debug': false,
-          'useBuiltIns': false
-        }
+          debug: false,
+          useBuiltIns: false,
+        },
       ],
       '@babel/preset-react',
       '@babel/typescript',
     ],
     plugins: [
       '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-transform-runtime',
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          absoluteRuntime: false,
+          corejs: false,
+          helpers: true,
+          regenerator: true,
+          useESModules: false,
+          version: '7.0.0-beta.0',
+        },
+      ],
       [
         '@babel/plugin-proposal-class-properties',
         {
-          'loose': true
-        }
+          loose: true,
+        },
       ],
       '@babel/plugin-transform-async-to-generator',
       '@babel/proposal-object-rest-spread',
       [
         'babel-plugin-inline-import',
         {
-          'extensions': ['.svg'],
+          extensions: ['.svg'],
         },
       ],
     ],
     env: {
       production: {
-        presets: [
-          'react-optimize'
-        ]
-      }
-    }
+        presets: ['react-optimize'],
+      },
+    },
   }
 }
